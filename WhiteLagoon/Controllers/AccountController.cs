0e413ca4f17d803b699ec98f76b73acf9d6eab4a
Phate <phanhuyduc0994@gmail.com>
2024-08-04 19:14:43 +0700

    fix route sign out

--- WhiteLagoon/Controllers/AccountController.cs
+++ WhiteLagoon/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace LibraryBook.Web.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "TTHS");
         }
         public IActionResult AccessDenied()
         {
